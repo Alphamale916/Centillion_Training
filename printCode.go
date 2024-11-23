0083655690b41143d7a8c88af8c86cfd10de7d53
MATHESH M <156519287+Alphamale916@users.noreply.github.com>
2024-11-23 21:13:53 +0530

    Add files via upload

---
+++ printCode.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main(){
+	// //Strings
+	var nameOne string = "Kumar"
+	var nameTwo = "Raj"
+	var nameThree string
+	fmt.Println(nameOne, nameTwo, nameThree)
+	nameOne = "Harish"
+	nameThree = "Ashok"
+	fmt.Println(nameOne, nameTwo, nameThree)
+	nameFour :="Selvan"
+	fmt.Println(nameFour)
+
+	//Int
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits & memory
+	var numOne int8 25
+	var numTwo int8-128
+	var numThree uint16 = 256
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 883759237594363564868547567.7
+}
